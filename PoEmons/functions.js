7f001b695793f08ae8caa91a4127d3cdf35cd88e
WikingLaptop <gargamel.ftn@gmail.com>
2018-02-20 15:09:25 +0100

    Progress!

---
+++ PoEmons/functions.js
@@ -0,0 +1,19 @@
+//calculates the safe zone for leveling 100% xp multiplier
+function safeZone (parameterLevel) {
+  return floor(3 + parameterLevel / 16);
+}
+//calculates effective difference between player level and zone level
+function effectiveDifference (parLevel,parZone) {
+  return max(abs(parLevel - parZone) - safeZone(parZone), 0);
+}
+//calculates xp multiplier
+function xpMultiplierLow (parLevel,parZone) {
+  let temp1 = parLevel+5;
+  let temp2 = parLevel+5+ Math.pow(safeZone(parZone),2.5);
+  let temp3 = Math.pow(temp1/temp2,1.5);
+  return max(temp3,0.01)
+}
+function xpMultiplierHigh (parLevel,parZone) {
+  let temp = xpMultiplierLow(parLevel,parZone) * 1/(1+0.1*(parLevel-94));
+  return max(temp,0.01)
+}
--- PoEmons/sketch.js
+++ PoEmons/sketch.js
@@ -1,40 +1,41 @@
 "use strict";
+var lvlSlider,zoneSlider,xpSlider;
 
 function setup() {
 
+	createCanvas(840,525);
+	background(155);
+	frameRate(60);
 
-	console.log(xpToGain);
-	//safe zone based on current level
-	var currentLevel = 32;
-	var safeZone = floor(3 + currentLevel / 16);
-	//console.log(safeZone);
-
-	//effective difference
-	var zoneLevel = 45;
-	var effectiveDifference = max(abs(currentLevel - zoneLevel) - safeZone, 0);
-	//console.log(effectiveDifference);
-
-	//calculating xp multiplier
-	var xpMultiplier;
-	var xpMultiplierPart;
-	if (currentLevel < 95) {
-		xpMultiplier = 1;
-	} else {
-		xpMultiplier = 2;
-	}
-	console.log(5);
+	lvlSlider = createSlider(1, 100, 1);
+  lvlSlider.position(20, 20);
+  zoneSlider = createSlider(1, 100, 1);
+  zoneSlider.position(20, 50);
+  xpSlider = createSlider(1, 1000, 1);
+  xpSlider.position(20, 80);
 
 }
 
 function draw() {
 
+	var currentLevel = lvlSlider.value();
+	var zoneLevel = zoneSlider.value();
+	var xpPerHour = xpSlider.value();
+	text("Player Level: " +currentLevel, lvlSlider.x * 2 + lvlSlider.width, 35);
+  text("Zone Level: " +zoneLevel, zoneSlider.x * 2 + zoneSlider.width, 65);
+  text("XP Per Hour: " +xpPerHour, xpSlider.x * 2 + xpSlider.width, 95);
+
+	//TODO: Make a function that calculates xp needed till target level.
+	//TODO: Make a pressable button that calculates xp till targeted level.
+	//TODO: Figure out how to clear screen when moving sliders.
+
 }
 
 /*
 TODO:
--calculate xp multiplier
--add currentXp[] - nextLevelXp[]
--triple array?? playerLevels [1-100][currentXp][nextLevelXp]; or object with current/next xp on each level?
+//-calculate xp multiplier
+//-add currentXp[] - nextLevelXp[]
+//-triple array?? playerLevels [1-100][currentXp][nextLevelXp]; or object with current/next xp on each level?
 -rawxp = xpperhour * xpmultiplier
 -100 - current level, how much xp for 100?
 -effective difference and xpmultiplier will have to be FUNCTIONS! to call each time for each level till 100
