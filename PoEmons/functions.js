dcbc6b730f5c82ed149fed0f96f8a8cde8ebee71
Wiking <gargamel.ftn@gmail.com>
2018-02-20 22:13:03 +0100

    ITS ALIVEEEEE

--- PoEmons/functions.js
+++ PoEmons/functions.js
@@ -1,37 +1,35 @@
 //Safe zone for leveling 100% xp multiplier
 function safeZone (currentLevel) {
-  return floor(3 + currentLevel / 16);
+     return floor(3 + currentLevel / 16);
 }
 //Effective difference between player level and zone level
 function effectiveDifference (currentLevel,zoneLevel) {
-  return max(abs(currentLevel - zoneLevel) - safeZone(zoneLevel), 0);
+     return max(abs(currentLevel - zoneLevel) - safeZone(zoneLevel), 0);
 }
 //Xp multiplier
 function xpMultiplier (currentLevel,zoneLevel) {
-  if (currentLevel<95){
-  let temp1 = currentLevel + 5;
-  let temp2 = currentLevel + 5 + Math.pow(effectiveDifference(currentLevel,zoneLevel),2.5);
-  let total = Math.pow(temp1/temp2,1.5);
-  return max(total,0.01);
-  } else {
-  let temp1 = currentLevel + 5;
-  let temp2 = currentLevel + 5 + Math.pow(effectiveDifference(currentLevel,zoneLevel),2.5);
-  let temp3 = 1/(1+0.1*(currentLevel-94));
-  let total = Math.pow(temp1/temp2,1.5)*temp3;
-  return max(total,0.01);
-  }
+     let temp1 = currentLevel + 5;
+     let temp2 = currentLevel + 5 + Math.pow(effectiveDifference(currentLevel,zoneLevel),2.5);
+     if (currentLevel<95){
+          let total = Math.pow(temp1/temp2,1.5);
+          return max(total,0.01);
+     } else {
+          let temp3 = 1/(1+0.1*(currentLevel-94));
+          let total = Math.pow(temp1/temp2,1.5)*temp3;
+          return max(total,0.01);
+     }
 }
 //Pure XP gained before diminishing returns
 function calculateRawXp(currentLevel,zoneLevel,xpPerHour){
-  return (xpPerHour/xpMultiplier(currentLevel,zoneLevel));
+     return (xpPerHour/xpMultiplier(currentLevel,zoneLevel));
 }
 //Hours needed per level till 100 returned in an array
 function calculateTime(currentLevel,zoneLevel,xpPerHour,xpToGain){
-  let tempArray = ['dummy'];
-  let tempRawXp = calculateRawXp(currentLevel,zoneLevel,xpPerHour);
-  for(i=currentLevel;i<100,i++;){
-    total = xpToGain[i]/(tempRawXp*xpMultiplier(i,zoneLevel));
-    tempArray.push(total);
-  }
-  return tempArray;
+     let tempArray = [];
+     let tempRawXp = calculateRawXp(currentLevel,zoneLevel,xpPerHour);
+     for(i=currentLevel-1;i<99;i++){
+          total = xpToGain[i]/(tempRawXp*xpMultiplier(i,zoneLevel));
+          tempArray.push(total);
+     }
+     return tempArray;
 }
--- PoEmons/sketch.js
+++ PoEmons/sketch.js
@@ -3,6 +3,7 @@ var lvlSlider,zoneSlider,xpSlider;
 var currentLevel,zoneLevel,xpPerHour;
 var timeArray;
 var globalbackground = [255,55,55];
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
 function setup() {
 
@@ -12,23 +13,23 @@ function setup() {
 	textSize(32);
 
 	lvlSlider = createSlider(1, 100, 1);
-  lvlSlider.position(20, 20);
-  zoneSlider = createSlider(1, 100, 1);
-  zoneSlider.position(20, 50);
-  xpSlider = createSlider(1, 1000, 1);
-  xpSlider.position(20, 80);
+	lvlSlider.position(20, 20);
+	zoneSlider = createSlider(1, 100, 1);
+	zoneSlider.position(20, 50);
+	xpSlider = createSlider(1, 100, 1);
+	xpSlider.position(20, 80);
 
 }
 
 function draw() {
 
-	if (currentLevel != lvlSlider.value() || zoneLevel != zoneSlider.value() || xpPerHour != xpSlider.value()) {
+	if (currentLevel != lvlSlider.value() || zoneLevel != zoneSlider.value() || xpPerHour != xpSlider.value()*10000000) {
 		clear();
 		background(globalbackground);
 		currentLevel = lvlSlider.value();
 		zoneLevel = zoneSlider.value();
-		xpPerHour = xpSlider.value();
-		//timeArray = calculateTime(currentLevel,zoneLevel,xpPerHour,xpToGain);
+		xpPerHour = xpSlider.value()*10000000;
+		timeArray = calculateTime(currentLevel,zoneLevel,xpPerHour,xpToGain);
 		//TEST
 		console.log('==========================');
 		console.log('Safe Zone:'+safeZone(currentLevel));
@@ -36,13 +37,14 @@ function draw() {
 		console.log('xpMulti:'+xpMultiplier(currentLevel,zoneLevel));
 		console.log('rawXp:'+calculateRawXp(currentLevel,zoneLevel,xpPerHour));
 		console.log(timeArray);
+		console.log('Total Hours till 100: ' +timeArray.reduce(reducer));
 		console.log('==========================');
 		//TEST
 	}
 
 	text("Player Level: " +currentLevel, lvlSlider.x * 2 + lvlSlider.width, 35);
-  text("Zone Level: " +zoneLevel, zoneSlider.x * 2 + zoneSlider.width, 65);
-  text("XP Per Hour: " +xpPerHour, xpSlider.x * 2 + xpSlider.width, 95);
+	text("Zone Level: " +zoneLevel, zoneSlider.x * 2 + zoneSlider.width, 65);
+	text("XP Per Hour: " +xpPerHour/1000000 + 'M', xpSlider.x * 2 + xpSlider.width, 95);
 	text("DETO SEND HELP SHIT IS ON FIRE YOOO" , 20, 200);
 	text("(●´ω｀●)ゞ(●´ω｀●)ゞ(●´ω｀●)ゞ(●´ω｀●)ゞ" , 20, 230);
 
